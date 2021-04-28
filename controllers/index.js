b3ce30cf02fe5fc0fbbc28e1b6d21fd98cd746e7
Jake <jakech617@gmail.com>
2021-04-28 18:48:20 -0400

    declared values for apiRoutes, and dashboardRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboardRoutes');
 const homeRoutes = require('./homeRoutes');
 
+
+router.use('/', apiRoutes);
+router.use('/', dashboardRoutes);
 router.use('/', homeRoutes);
+
 
 module.exports = router;
