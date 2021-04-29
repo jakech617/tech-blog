c205bc9aed0da10304d47322f3d076324b76f159
Jake <jakech617@gmail.com>
2021-04-29 11:57:02 -0400

    date formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,10 @@
 module.exports = {
+    format_date: (date) => {
+        
+        return date.toLocaleDateString();
+      },
+      format_amount: (amount) => {
 
+        return parseInt(amount).toLocaleString();
+      },
 };
